e46d8d33f4362e58cd75e48488e35a19de8f2599
pinchiguillo <60063206+pinchiguillo@users.noreply.github.com>
2024-12-04 10:10:52 +0100

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+// Created by pinchiguillo on 04/12/2024.
+//
+
+#include <iostream>
+#include <locale>
+
+int main() {
+
+    setlocale(LC_ALL, "es_ES.UTF-8");
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
